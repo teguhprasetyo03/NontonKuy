4a745e16dd74d85ab6997b8ceb7022f76825eb3e
TeguhPrasetyo03 <teguhprasetyo261211@gmail.com>
2020-01-26 14:55:27 +0700

    menambahkan project ke github

---
+++ app/src/main/java/com/studiomasteguh/nontonkuy/onboarding/OnBoardingOneActivity.kt
@@ -0,0 +1,13 @@
+package com.studiomasteguh.nontonkuy.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.studiomasteguh.nontonkuy.R
+
+class OnBoardingOneActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding_one)
+    }
+}
