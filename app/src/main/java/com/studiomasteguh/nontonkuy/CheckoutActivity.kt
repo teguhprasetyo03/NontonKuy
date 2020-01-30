6273648ae00a1dff63110d38a3309eebbf80cb96
Teguhprasetyo03 <teguhprasetyo261211@gmail.com>
2020-01-30 15:57:11 +0700

    menambahkan checkout page

---
+++ app/src/main/java/com/studiomasteguh/nontonkuy/CheckoutActivity.kt
@@ -0,0 +1,12 @@
+package com.studiomasteguh.nontonkuy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkout)
+    }
+}
