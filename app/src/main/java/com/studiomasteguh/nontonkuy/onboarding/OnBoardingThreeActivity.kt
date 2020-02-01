37cfca7588ad5e2bc14a8d3ae4abce83cd0fba8b
Teguhprasetyo03 <teguhprasetyo261211@gmail.com>
2020-02-02 00:19:11 +0700

    membuat splashscreen dan onboarding

--- app/src/main/java/com/studiomasteguh/nontonkuy/onboarding/OnBoardingThreeActivity.kt
+++ app/src/main/java/com/studiomasteguh/nontonkuy/onboarding/OnBoardingThreeActivity.kt
@@ -5,23 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.studiomasteguh.nontonkuy.R
 import com.studiomasteguh.nontonkuy.signin.SignInActivity
-import kotlinx.android.synthetic.main.activity_on_boarding_one.*
+import kotlinx.android.synthetic.main.activity_on_boarding_three.*
 
-class OnBoardingOneActivity : AppCompatActivity() {
+class OnBoardingThreeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_on_boarding_one)
-
-        // btn home berfungsi untuk berpindah dari
-        // on boarding one ke on boarding two
-        btn_home.setOnClickListener {
-            val intent = Intent(this, OnBoardingTwoActivity::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.activity_on_boarding_three)
 
         // jika kita mengklik lewati perkenalan
-        btn_daftar.setOnClickListener {
+        btn_home_ob3.setOnClickListener {
             finishAffinity()
 
             val intent = Intent(this, SignInActivity::class.java)
--- app/src/main/java/com/studiomasteguh/nontonkuy/onboarding/OnBoardingTwoActivity.kt
+++ app/src/main/java/com/studiomasteguh/nontonkuy/onboarding/OnBoardingTwoActivity.kt
@@ -1,13 +1,21 @@
 package com.studiomasteguh.nontonkuy.onboarding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.studiomasteguh.nontonkuy.R
+import kotlinx.android.synthetic.main.activity_on_boarding_two.*
 
 class OnBoardingTwoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_boarding_two)
+
+        // pindah ke activity on boarding 3
+        btn_home_ob2.setOnClickListener {
+            val intent = Intent(this, OnBoardingThreeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
