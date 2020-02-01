e2de4267126e0db292af086d7461a6e651e5ec9c
Teguhprasetyo03 <teguhprasetyo261211@gmail.com>
2020-02-01 23:46:58 +0700

    menambahkan edit profile , history dan wallet

--- app/src/main/java/com/studiomasteguh/nontonkuy/SplashScreenActivity.kt
+++ app/src/main/java/com/studiomasteguh/nontonkuy/SplashScreenActivity.kt
@@ -1,13 +1,24 @@
 package com.studiomasteguh.nontonkuy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
+import android.os.Handler
+import com.studiomasteguh.nontonkuy.onboarding.OnBoardingOneActivity
 
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        // splash screen
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this , OnBoardingOneActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
