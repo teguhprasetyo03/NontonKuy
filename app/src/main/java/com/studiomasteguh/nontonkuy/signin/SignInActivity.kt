04250f7b73a6265d3b51b80e0b0574021c62c2ac
Teguhprasetyo03 <teguhprasetyo261211@gmail.com>
2020-02-02 18:51:53 +0700

    menambahkan firebase

--- app/src/main/java/com/studiomasteguh/nontonkuy/signin/SignInActivity.kt
+++ app/src/main/java/com/studiomasteguh/nontonkuy/signin/SignInActivity.kt
@@ -2,12 +2,37 @@ package com.studiomasteguh.nontonkuy.signin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.studiomasteguh.nontonkuy.R
 
+import com.google.firebase.database.FirebaseDatabase
+
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.ValueEventListener
+import android.util.Log
 class SignInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        setContentView(com.studiomasteguh.nontonkuy.R.layout.activity_sign_in)
+
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello Kadal")
+
+        // Read from the database
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue(String::class.java)
+                Log.d("tampan", "Value is: " + value!!)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("tamvan sekali", "Failed to read value.", error.toException())
+            }
+        })
     }
 }
