d64b906a06a1d57070a1ab19560b9da1bdef0608
Teguhprasetyo03 <teguhprasetyo261211@gmail.com>
2020-02-25 23:13:29 +0700

    menambahkan tiket detail

---
+++ app/src/main/java/com/studiomasteguh/nontonkuy/home/tiket/TiketActivity.kt
@@ -0,0 +1,12 @@
+package com.studiomasteguh.nontonkuy.home.tiket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TiketActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tiket)
+    }
+}
---
+++ app/src/main/java/com/studiomasteguh/nontonkuy/home/tiket/TiketAdapter.kt
@@ -0,0 +1,2 @@
+package com.studiomasteguh.nontonkuy.home.tiket
+
